54a0d939b353518e88e46b126306aed7eb50bd0c
faustojc <faustojcboko@gmail.com>
2023-03-19 20:36:06 +0800

    Using cache in retrieving the data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,35 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private ?int $old = null;
+    private ?int $curr = null;
+
     /**
      * Register any application services.
      */
     public function register()
     {
-        //
+        $this->app->singleton('customer_id', function () {
+            if (Auth::guard('customer')->check()) {
+                $this->curr = Cache::get('customer_id');
+            }
+
+            if (is_null($this->curr) && !is_null($this->old)) {
+                return $this->old;
+            } elseif (is_null($this->old) && is_null($this->curr)) {
+                return null;
+            }
+
+            $this->old = $this->curr;
+
+            return $this->curr;
+        });
     }
 
     /**

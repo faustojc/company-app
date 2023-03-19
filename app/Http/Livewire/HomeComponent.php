0ba9ab387e38390dd3f1cfc33b1103dabdecace5
faustojc <faustojcboko@gmail.com>
2023-03-19 20:41:28 +0800

    Removed some code and extending the Main Component

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -5,25 +5,16 @@ namespace App\Http\Livewire;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
-use \Symfony\Component\HttpFoundation\Request;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeComponent extends Component
 {
-    public $customer;
-    public $orders;
-
-    public function render(Request $request): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    public function render(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $id = $request->getSession()->get('data');
+        $main = new MainComponent();
+        $main = $main->render_main();
 
-        $this->customer = DB::table('customer')->where('customer_id', $id)->get()->first();
-        $this->orders = DB::table('order')->where('customer_id', $id)->get()->all();
-
-        return view('livewire.home-component', [
-            'customer' => $this->customer,
-            'orders' => $this->orders
-        ])->extends('layouts.master');
+        return view('livewire.home-component', $main)->extends('livewire.main-component');
     }
 }

c6bafdd3259cb2faeed05b07a0d8f27276dc1fd7
yujim16 <yuji17moronaga@gmail.com>
2023-03-20 00:10:14 +0800

    home

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,5 +1,12 @@
 @extends('livewire.main-component')
 
 @section('content')
-    Home
+<section id="home" class="bg-no-repeat object-cover bg-top" style="background-image: url('resource/images/bkg.jpg');">
+    <div class="container">
+        <h5>Delargo.ph</h5>
+        <h1><span>Best Prices</span> This Year</h1>
+        <p>Offers your very comfortable time on walking and exercises. </p>
+        <button>Shop Now</button>
+    </div>
+</section>
 @endsection

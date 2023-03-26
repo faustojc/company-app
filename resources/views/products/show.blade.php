df7776c5f5c9245b82bb640573e03f64504ad46c
faustojc <faustojcboko@gmail.com>
2023-03-26 10:29:56 +0800

    Added related products

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -23,7 +23,7 @@
                     <p>Flaws: {{ $product->flaw }}</p>
                 </div>
                 <hr>
-                <form method="POST" action="{{ route('orders.store', $product)}}" class="d-flex"> @csrf
+                <form method="POST" action="{{ route('orders.store', ['product' => $product])}}" class="d-flex"> @csrf
                     <div class="mb-3">
                         <input type="number" id="quantity" name="quantity" value="1" min="1" max="99" class="form-control rounded-0 px-2 py-1">
                     </div>
@@ -34,6 +34,35 @@
                         </button>
                     </div>
                 </form>
+            </div>
+        </div>
+    </section>
+    <section class="pt-5 w-100">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-12 text-center">
+                    <h4 class="title">Related Products</h4>
+                </div>
+                <div class="d-flex flex-wrap justify-content-center pt-5">
+
+                    @foreach($relatedProducts as $related)
+                        <div class="card rounded-0 mx-2 mb-4" style="width: 19rem">
+                            <a href="{{ route('products.show', $related) }}">
+                                <img src="{{ $related->filepath . $related->filename }}" class="card-img-top mb-2" alt="{{ $related->name }}" />
+                            </a>
+                            <div class="card-body">
+                                <p class="card-title text-uppercase m-0 sub-title">{{ $related->category }}</p>
+                                <a href="{{ route('products.show', $related) }}" class="fw-bold text-body product-name text-decoration-none">{{ $related->name }}</a>
+                                <p class="text-success sub-title">P{{ $related->price }}.00</p>
+                                <a href="{{ route('products.show', $related) }}" class="btn btn-outline-secondary rounded-0 text-uppercase px-2 py-1">
+                                    <span class="bi bi-cart me-2"></span>
+                                    Add to Cart
+                                </a>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
             </div>
         </div>
     </section>

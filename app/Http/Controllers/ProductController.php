be0fcc270e5f605aed62a76efb78a2ea9ef6fb98
faustojc <faustojcboko@gmail.com>
2023-05-24 01:04:56 +0800

    Removed unused routes, added DataController in sharing data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customer;
-use App\Models\Order;
 use App\Models\Product;
-use App\Controller\HomeComponent;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -15,58 +10,23 @@ class ProductController extends Controller
 {
     public function index(): Response
     {
-        $customer = Customer::query()->where('customer_id', app('customer_id'))->get()->first();
-        $orders = Order::query()->where('customer_id', app('customer_id'))->get();
-        $products = Product::all();
+        DataController::shareData();
 
         return Inertia::render('Product/List', [
-            'customer' => $customer,
-            'orders' => $orders,
-            'products' => $products
+            'products' => Product::all()
         ]);
     }
 
-    // For the seller
-    public function create()
+    public function show($product_id): Response
     {
-        return view('products.create');
-    }
+        DataController::shareData();
 
-    // For the seller
-    public function store(Request $request)
-    {
-        return view('products.store');
-    }
-
-    public function show(Product $product): Response
-    {
-        $id = app('customer_id');
-
-        $customer = Customer::query()->where('customer_id', $id)->first();
-        $orders = Order::query()->where('customer_id', $id)->get();
-        $relatedProducts = Product::query()->where('category', $product->category)->get()->except($product->product_id);
+        $product = Product::query()->where('product_id', $product_id)->first();
+        $relatedProducts = Product::query()->where('category', $product->category)->get()->except($product_id);
 
         return Inertia::render('Product/Show', [
-            'customer' => $customer,
-            'orders' => $orders,
             'product' => $product,
             'relatedProducts' => $relatedProducts
         ]);
-    }
-
-    // For the seller
-    public function edit(Product $product)
-    {
-        return view('products.edit', ['product' => $product]);
-    }
-
-    public function update(Request $request, Product $product)
-    {
-        return view('products.update', $product)->extends('livewire.main-component');
-    }
-
-    public function destroy(Product $product)
-    {
-        return view('products.destroy', $product)->extends('livewire.main-component');
     }
 }

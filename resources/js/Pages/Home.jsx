1aea440607372542ef452d175c08840536f75dbe
faustojc <faustojcboko@gmail.com>
2023-05-24 00:59:06 +0800

    Removed unused imports and fix routing

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import route from "ziggy-js/src/js";
-import { Link, usePage } from "@inertiajs/react";
-import Main from "@/Pages/Layouts/Main";
+import { Link } from "@inertiajs/react";
+import Navbar from "@/Components/Navbar";
+import OffcanvasOrders from "@/Components/OffcanvasOrders";
+import Footer from "@/Components/Footer";
 
 function Hero() {
     return (
@@ -41,18 +43,18 @@ function Content({products}) {
                     </div>
                     <div className="d-flex flex-wrap justify-content-center pt-5">
 
-                        {products.slice(0, 4).map((product) => (
+                        {products.map((product) => (
                             <div key={ product.product_id } className="card rounded-0 mx-2 mb-4" style={{ width: '19rem' }}>
-                                <Link href={ route('products.show', [product]) }>
+                                <Link href={ route('products.show', [product.product_id]) }>
                                     <img src={product.filepath + product.filename} className="card-img-top mb-2" alt={ product.name } />
                                 </Link>
                                 <div className="card-body">
                                     <p className="card-title text-uppercase m-0 sub-title">{ product.category }</p>
-                                    <Link href={ route('products.show', [product]) } className="fw-bold text-body product-name text-decoration-none">
+                                    <Link href={ route('products.show', [product.product_id]) } className="fw-bold text-body product-name text-decoration-none">
                                         {product.name}
                                     </Link>
                                     <p className="text-success sub-title">P {  parseFloat(product.price).toFixed(2) }</p>
-                                    <Link href={ route('products.show', [product]) } className="btn btn-outline-secondary rounded-0 text-uppercase px-2 py-1">
+                                    <Link href={ route('products.show', [product.product_id]) } className="btn btn-outline-secondary rounded-0 text-uppercase px-2 py-1">
                                         <span className="bi bi-cart me-2"></span>
                                         Add to Cart
                                     </Link>
@@ -93,15 +95,20 @@ function Feature() {
     )
 }
 
-function Home() {
-    const { products } = usePage().props;
-
+function Home({ products }) {
     return (
-        <Main>
-            <Home.Hero />
-            <Home.Content products={products} />
-            <Home.Feature />
-        </Main>
+        <>
+            <Navbar />
+            <OffcanvasOrders />
+
+            <div className="container-fluid">
+                <Home.Hero />
+                <Home.Content products={products} />
+                <Home.Feature />
+            </div>
+
+            <Footer />
+        </>
     );
 }
 
